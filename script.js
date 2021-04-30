1085159960be8fae17705e092d331a9e8d7e2a30
moacirdavidag <moacirdavidag@gmail.com>
2021-04-30 17:53:19 -0300

    navbar dropdown for mobile made

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ const buttonAbout = document.getElementById("about")
 const buttonPortfolio = document.getElementById("portfolio")
 const buttonContact = document.getElementById("contact")
 const buttonLogo = document.getElementById("logo")
+const navButton = document.getElementById("btnNav_mobile");
 
 const contentSpace = document.getElementById("section")
 
 let currentPage = 1
 
 // HTML Content
-// const homeHTML = `<h1>Texto de Inicio</h1>`
+ /* const homeHTML = `<section class="section" id="section">
+      <img src="computerhome.png" alt="computer_homeimg" class="box_img" />
+      <h1 id="home_h1">SOLUÇÕES EM TECNOLOGIA PARA FACILITAR O SEU DIA A DIA.</h1>
+      <a href="#"><input type="button" id="home_button" value="CONHEÇA NOSSOS PRODUTOS" /></a>
+    </section>
+    <section class="section">
+      <img class="box_img" src="2.png" alt="assitencia_img" />
+      <h1>ASSISTÊNCIA TÉCNICA 24H/7</h1>
+      <h3>Te ajudamos no que precisar. É só chamar e nossa equipe estará te assistindo em suas necessidades!</h3>
+    </section>
+    <section class="section" id="section">
+      <img class="box_img" src="3.png" alt="digital_img" />
+      <h1>DIGITALIZE O SEU NEGÓCIO</h1>
+      <h3>Nossos produtos são capazes de conectar sua empresa ao mundo digital e impulsionar a sua presença no mercado.</h3>
+    </section>
+    <section class="section">
+      <img class="box_img" src="4.png" alt="virtual_img" />
+      <h1>NÃO PERCA MAIS CLIENTES</h1>
+      <h3>Ofereça os seus produtos em qualquer hora e para clientes de qualquer local! A Kavutech oferece softwares especializados em vendas que alavancarão o seu comércio.</h3>
+    </section>`; */
+    
 const aboutHTML = `<h1>Texto de Sobre</h1>`
 const portfolioHTML = `<h1>Texto de Portfolio</h1>`
 const contactHTML = `<h1>Texto de Contato</h1>`
@@ -30,3 +51,20 @@ buttonContact.onclick = () => {
 }
 
 buttonHome.click()
+
+// Display nav do mobile
+let count = 0; // auxiliar pra fechar o menu
+
+navButton.onclick = () => { 
+  let body = document.body;
+  body.style.overflow = "hidden";
+  count++;
+  const menuNav = document.getElementById("mobile_nav");
+  menuNav.style.display = "block";
+  if(count == 2) {
+    menuNav.style.display = "none";
+    body.style.overflow = "scroll";
+    count = 0;
+  }
+
+}
